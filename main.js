581d5af54d77ceb2c9f9fae3854dfb506c599e97
GaddzGZ <66917817+GaddzGZ@users.noreply.github.com>
2020-11-09 21:10:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const rulesChannelId = '775440595458981949'
+
+client.once('ready', () => {
+	console.log('Bot is online')
+
+});
+
+client.on('guildMemberAdd', member => {
+
+	const channel = member.guild.channels.cache.find(channel => channel.name === "general");
+	if (!channel) return;
+
+	channel.send(`Welcome to our server, ${member}, please read the ${member.guild.channels.cache.get(rulesChannelId).toString()} in rules channel!`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('Nzc0NzY1OTYzMzU3ODQ3NTYy.X6cjCA.5KpT38T54sQVDqqdKHFY4G9IePc');
