005b81360a8083325ac983f85be898ea9530cc69
GaddzGZ <66917817+GaddzGZ@users.noreply.github.com>
2020-11-11 21:08:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,18 +5,19 @@ const rulesChannelId = '775440595458981949'
 client.once('ready', () => {
 	console.log('Bot is online')
 
-});
+});               // WELCOME MESSAGE START
 
 client.on('guildMemberAdd', member => {
-	
+	const messageWelcome = `Welcome tdfso oour server, ${member}, please read the ${member.guild.channels.cache.get(rulesChannelId).toString()} in rules channel!`
+
+
 	const channel = member.guild.channels.cache.find(channel => channel.name === "general");
 	if (!channel) return;
 	setTimeout(welcomeMessage, 500)
-	function welcomeMessage(){
-		channel.send(`Welcome to our server, ${member}, please read the ${member.guild.channels.cache.get(rulesChannelId).toString()} in rules channel!`);
-	}
-});
-
+	function welcomeMessage() {
+		channel.send(messageWelcome);
+	}	
+});             // WELCOME MESSAGE END
 
 
 
