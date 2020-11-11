91f902cb5494fb8b76912d8a3da36a061dc15576
GaddzGZ <66917817+GaddzGZ@users.noreply.github.com>
2020-11-11 22:45:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const rulesChannelId = '775440595458981949'
+const rulesChannelId = '774414949916868628'
 
 client.once('ready', () => {
 	console.log('Bot is online')
@@ -8,10 +8,10 @@ client.once('ready', () => {
 });               // WELCOME MESSAGE START
 
 client.on('guildMemberAdd', member => {
-	const messageWelcome = `Welcome the server, ${member}, please read the ${member.guild.channels.cache.get(rulesChannelId).toString()} in rules channel!`
+	const messageWelcome = `Welcome to the Orbital Nine Games server, ${member}! Please read the ${member.guild.channels.cache.get(rulesChannelId).toString()}`
 
 
-	const channel = member.guild.channels.cache.find(channel => channel.name === "general");
+	const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
 	if (!channel) return;
 	setTimeout(welcomeMessage, 500)
 	function welcomeMessage() {
